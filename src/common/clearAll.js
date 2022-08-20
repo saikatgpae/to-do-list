b989be0c545fbbabd9bf0f62e672280593a5e0dc
Saikat Chakraborty <saikatgpae@gmail.com>
2022-08-20 19:20:07 +0530

    Add delete function.

--- src/common/clearAll.js
+++ src/common/clearAll.js
@@ -1,5 +1,7 @@
 const clearAll = () => {
-  // Remove all checked
+  const storedList = JSON.parse(localStorage.getItem('toDolist'));
+  const newArray = storedList.filter((item) => item.completed === false);
+  localStorage.setItem('toDolist', JSON.stringify(newArray));
   window.location.reload();
 };
 
